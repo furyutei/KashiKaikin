d4a3bebfd4ef54c7bf3e8451f2659383e00c56f2
furyu <furyutei@gmail.com>
2015-07-07 21:11:44 +0900

    0.1.0.0: 初版

---
+++ KashiKaikin.user.js
@@ -0,0 +1,316 @@
+// ==UserScript==
+// @name            KashiKaikin
+// @namespace       http://d.hatena.ne.jp/furyu-tei
+// @author          furyu
+// @version         0.1.0.0
+// @include         http://*
+// @include         https://*
+// @description     歌詞検索サイトの歌詞テキストをコピー可能にする
+// ==/UserScript==
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 furyu <furyutei@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+(function(w, d) {
+
+var GLOBAL_OPTIONS = {
+    NAME_SCRIPT : 'KashiKaikin'
+,   DEBUG : true
+,   KASHI_CSS : {
+        'border' :'double 3px orange'
+    ,   'padding' : '4px 6px'
+    ,   'text-align' : 'left'
+    ,   'line-height' : '1.4'
+    ,   'font-size' : '14px'
+    ,   'font-family' : '"Hiragino Kaku Gothic ProN", Meiryo, sans-serif, monospace'
+    }
+};
+
+if ((w !== w.parent) || d.getElementById(('__user_script__' + GLOBAL_OPTIONS.NAME_SCRIPT))) {return;}
+
+var site_infomations = [
+    { // ■ 歌詞検索サービス　歌ネット
+        reg_url : '^https?://(?:www\\.)?uta-net\\.com/song/[^/]+/'
+    ,   sample_url : 'http://www.uta-net.com/song/185828/'
+    ,   options : {
+            jquery : false
+        }
+    ,   main : function(w, d, global_options, options) {
+            var $ = $j;
+            $.get(
+                $('span#ipad_kashi img:first').attr('src'),
+                function(xml) {
+                    var chunks = [], elm = $('<pre/>');
+                    $(xml).find('text').each(function() {
+                        chunks.push($(this).text());
+                    });
+                    elm.text(chunks.join('\n')).css(global_options.KASHI_CSS);
+                    $('p#flash_area').before(elm);
+                },
+                'xml'
+            );
+            $('body').unbind('contextmenu');
+        }
+    }
+
+,   { // ■ うたまっぷ 歌詞を無料で検索表示
+        reg_url : '^https?://(?:(?:kids|www)\\.)?utamap\\.com/(?:pc/)?(?:show|view)kasi(?:_pc)?\\.php\\?surl=.+'
+    ,   sample_url : 'http://www.utamap.com/showkasi.php?surl=k-150520-218'
+    ,   options : {
+            jquery : true
+        }
+    ,   main : function(w, d, global_options, options) {
+            var $ = w.$, url = w.location.href;
+            $.get(
+                url.match(/^https?:\/\/kids\./) ? 'js_smt_pc.php' : 'js_smt.php',
+                {
+                    unum : url.match(/[?&]surl=([^?&]+)/)[1]
+                },
+                function(script) {
+                    var chunks = [], elm = $('<pre/>');
+                    script.replace(
+                        /\.fillText\(.*?'((?:\\'|[^'])*)'.*?\)/g,
+                        function(m, chunk) {
+                            chunks.push(chunk.replace(/\\'/g, "'"));
+                            return m;
+                        }
+                    );
+                    elm.text(chunks.join('\n')).css(global_options.KASHI_CSS);
+                    $('canvas#canvas2,object#showkasi').before(elm);
+                },
+                'html'
+            );
+        }
+    }
+
+,   { // ■ 歌詞検索サービス 歌詞ＧＥＴ
+        reg_url : '^https?://www\\.kget\\.jp/lyric/[^/]+/'
+    ,   sample_url : 'http://www.kget.jp/lyric/217912/'
+    ,   options : {
+            jquery : false
+        }
+    ,   main : function(w, d, global_options, options) {
+            d.body.onselectstart = function() {
+               return true;
+            };
+            d.body.oncontextmenu = function() {
+               return true;
+            };
+        }
+    }
+
+,   { // ■ 歌詞ナビ 無料歌詞検索サービス
+        reg_url : '^https?://kashinavi\\.com/song_view\\.html\?.+'
+    ,   sample_url : 'http://kashinavi.com/song_view.html?86577'
+    ,   options : {
+            jquery : true
+        }
+    ,   main : function(w, d, global_options, options) {
+            var $ = w.$;
+            $.post(
+                'http://kashinavi.com/cgi-bin/kashi.cgi',
+                {
+                    kdifoe88: 'smx;paa',
+                    file_no: $('link[rel="canonical"]:first').attr('href').match(/\/song_view\.html\?(\d+)/)[1]
+                },
+                function(result) {
+                    var elm = $('<pre/>');
+                    elm.text(result.replace(/^kashi=.*?\n+/,'')).css(global_options.KASHI_CSS);
+                    $('object#aaa').before(elm);
+                },
+                'html'
+            );
+        }
+    }
+
+,   { // ■ 歌詞検索J-Lyric.net
+        reg_url : '^https?://j-lyric\\.net/artist/[^/]+/[^.]+.html'
+    ,   sample_url : 'http://j-lyric.net/artist/a05a05d/l035fb5.html'
+    ,   options : {
+            jquery : true
+        }
+    ,   main : function(w, d, global_options, options) {
+            d.body.onselectstart = function() {
+               return true;
+            };
+            d.body.oncontextmenu = function() {
+               return true;
+            };
+        }
+    }
+
+,   { // ■ JOYSOUND.com
+        reg_url : '^https?://www\\.joysound\\.com/web/search/song/.+'
+    ,   sample_url : 'https://www.joysound.com/web/search/song/420767#lyrics'
+    ,   options : {
+            jquery : true
+        }
+    ,   main : function(w, d, global_options, options) {
+            $(d.body).css({
+                '-webkit-touch-callout' : 'default'
+            ,   '-webkit-user-select' : 'text'
+            ,   '-moz-user-select' : 'text'
+            ,   '-ms-user-select' : 'text'
+            ,   'user-select' : 'text'
+            });
+            
+            var enable_selection = function() {
+                //$(d.body).attr('onselectstart', 'return true').attr('oncontextmenu', 'return true');
+                d.body.onselectstart = function() {
+                   return true;
+                };
+                d.body.oncontextmenu = function() {
+                   return true;
+                };
+            };
+            
+            /*
+            //enable_selection();
+            //
+            //new MutationObserver(function(records) {
+            //    enable_selection();
+            //}).observe(d.body, {
+            //    attributes : true
+            //,   characterData : true
+            //,   childList : true
+            //});
+            */
+            
+            setInterval(enable_selection, 1000); // 一回だけだとタイミングによっては無効化されてしまう
+        }
+    }
+
+,   { // ■ J-Total Music!
+        reg_url : '^https?://music\\.j-total\\.net/data/.+'
+    ,   sample_url : 'http://music.j-total.net/data/021na/001_nagabuchi_tsuyoshi/005.html'
+    ,   options : {
+            jquery : false
+        }
+    ,   main : function(w, d, global_options, options) {
+            d.body.onselectstart = function() {
+               return true;
+            };
+            d.oncontextmenu = d.body.oncontextmenu = function() {
+               return true;
+            };
+        }
+    }
+
+,   { // ■ UtaTen 無料歌詞検索サイトの決定版！うたてん
+        reg_url : '^https?://utaten\\.com/lyric/.+'
+    ,   sample_url : 'http://utaten.com/lyric/%E5%8C%97%E5%AE%87%E6%B2%BB%E3%82%AB%E3%83%AB%E3%83%86%E3%83%83%E3%83%88/%E3%83%88%E3%82%A5%E3%83%83%E3%83%86%E3%82%A3%EF%BC%81/'
+    ,   options : {
+            jquery : true
+        }
+    ,   main : function(w, d, global_options, options) {
+            $('body,div.lyricBody').css({
+                '-webkit-touch-callout' : 'default'
+            ,   '-webkit-user-select' : 'text'
+            ,   '-moz-user-select' : 'text'
+            ,   '-ms-user-select' : 'text'
+            ,   'user-select' : 'text'
+            });
+            d.body.onselectstart = function() {
+               return true;
+            };
+            d.body.oncontextmenu = function() {
+               return true;
+            };
+        }
+    }
+
+/* // 雛形
+,   { // ■
+        reg_url : ''
+    ,   sample_url : ''
+    ,   options : {
+            jquery : true
+        }
+    ,   main : function(w, d, global_options, options) {
+        }
+    }
+*/
+];
+
+
+var call_main = function(w, d, main, global_options, options) {
+    if (!global_options) {global_options = {};}
+    if (!options) {options = {};}
+    
+    var is_jquery_valid = function() {
+        var $ = w.$;
+        return ($ && $.fn && $.fn.jquery);
+    };  //  end of is_jquery_valid
+    
+    if (!options.jquery || is_jquery_valid()) {
+        main(w, d, global_options, options);
+        return;
+    }
+    
+    var check = function() {
+        var $ = w.$;
+        if (is_jquery_valid()) {
+            main(w, d, global_options, options);
+            return;
+        }
+        setTimeout(check, 100);
+    };
+    check();
+};  //  end of call_main()
+
+
+var load_script = function(script_url, main, options, script_id) {
+    var script = d.createElement('script');
+    if (script_url) {
+        script.src = script_url;
+    }
+    if (main) {
+        if (!options) {options = {};}
+        if (options.jquery) {
+            load_script('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js');
+        }
+        script.textContent = '(' + call_main.toString() + ')(' + [
+            'window', 'document', main.toString(), JSON.stringify(GLOBAL_OPTIONS), JSON.stringify(options)
+        ].join(',') + ');';
+    }
+    if (script_id) {
+        script.id = script_id;
+    }
+    d.documentElement.appendChild(script);
+}; // end of load_script()
+
+
+var url = w.location.href;
+
+for (var ci = 0, len = site_infomations.length; ci < len; ci++) {
+    var site_information = site_infomations[ci];
+    if (url.match(site_information.reg_url)) {
+        load_script(null, site_information.main, site_information.options, '__user_script__' + GLOBAL_OPTIONS.NAME_SCRIPT);
+        break;
+    }
+}
+
+})(window, document);
+
+// ■ end of file
---
+++ README.md
@@ -0,0 +1,47 @@
+KashiKaikin
+===========
+【歌詞解禁】 歌詞検索サービスの歌詞テキストコピーを可能に  
+- License: The MIT license  
+- Copyright (c) 2015 風柳(furyu)  
+- 対象ブラウザ： Firefox（[Greasemonkey](https://addons.mozilla.org/ja/firefox/addon/greasemonkey/)が必要）、Google Chrome（[Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ja)が必要）(※いずれも PC 版のみ)  
+
+
+■ 歌詞解禁（KashiKaikin）とは？
+---
+歌詞検索サービスでは、歌詞のコピーを禁止するための措置が施されている場合がほとんどです。  
+このスクリプトは、いくつかの歌詞検索サイトにて、歌詞テキストのコピーを可能にするものです。  
+
+
+■ インストール方法
+---
+[Greasemonkey](https://addons.mozilla.org/ja/firefox/addon/greasemonkey/)を入れたFirefox、もしくは[Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=ja)を入れたGoogle Chromeにて、  
+
+> [KashiKaikin.user.js](https://github.com/furyutei/KashiKaikin/raw/master/KashiKaikin.user.js)  
+
+をクリックし、指示に従ってインストール。  
+
+
+■ 使い方
+---
+対応しているサービスにて歌詞を表示すると、歌詞テキストが選択・コピー可能となります。  
+
+※ 2015.07.07 現在の対応サービス  
+- [歌詞検索サービス　歌ネット](http://www.uta-net.com/)  
+- [うたまっぷ 歌詞を無料で検索表示](http://www.utamap.com/)  
+- [歌詞検索サービス 歌詞ＧＥＴ](http://www.kget.jp/)  
+- [歌詞ナビ 無料歌詞検索サービス](http://kashinavi.com/)  
+- [歌詞検索J-Lyric.net](http://j-lyric.net/)  
+- [JOYSOUND.com](https://www.joysound.com/web/)  
+- [J-Total Music!](http://music.j-total.net/)  
+- [UtaTen 無料歌詞検索サイトの決定版！うたてん](http://utaten.com/)  
+
+
+■ 注意事項
+---
+**歌詞の利用については、著作権法や各サービスの利用規約等に基づいた適切な範囲内で行ってください。**  
+**本スクリプトを使用した結果については、作者は一切の責任を負いません。**  
+
+
+■ 関連記事
+---
+- [歌詞検索サービスの歌詞(テキスト)コピー禁止手法について調べてみた - 風柳メモ](http://d.hatena.ne.jp/furyu-tei/20150706/1436182102)  
